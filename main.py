b3db05dc5c47938c6a9f63c0856375b99ec394e8
peterkush <pkungu004@gmail.com>
2021-12-07 18:47:25 +0300

    install tkinter and gave the dimensions of the phonebook

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+
+def main():
+    root = Tk()
+
+    root.geometry("650x550+350+20")
+    root.title("Phone Book Apps")
+    root.resizable(False,False)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
