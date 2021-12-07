5062e41623dd0ecbec2b74fb47fcf862e4a096d1
peterkush <pkungu004@gmail.com>
2021-12-07 23:13:30 +0300

    add tittle

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 
 from tkinter import *
+from datetime import date, datetime
+import datetime
+
+date = datetime.datetime.now().date()
+date = str(date)
 
 class Application(object):
      def __init__(self,master ):
          self.master=master
+
          #frame
          self.top = Frame(master,  height=150,bg='pink')
-         self.top.pack(fill='x')
+         self.top.pack(fill = X)
+
+         self.bottom = Frame(master,  height=500,bg='#84DFFF')
+         self.bottom.pack(fill = X)
+         self.heading =Label(self.top,text ='my phoneBook App',font='arial 15 bold ',bg='white',fg='gold')
+         
+         self.heading.place(x=230,y=50)
 
 
 
