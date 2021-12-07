287fe0559e4532327284f1fac2b5aa4922b57905
peterkush <pkungu004@gmail.com>
2021-12-07 19:54:58 +0300

    style the frame

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 
 from tkinter import *
 
+class Application(object):
+     def __init__(self,master ):
+         self.master=master
+         #frame
+         self.top = Frame(master,  height=150,bg='pink')
+         self.top.pack(fill='x')
+
+
+
 def main():
     root = Tk()
+    app=Application(root)
 
     root.geometry("650x550+350+20")
     root.title("Phone Book Apps")
